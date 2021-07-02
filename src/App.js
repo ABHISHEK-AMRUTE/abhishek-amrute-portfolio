33bafb43e999c415f4c00a01f28214a5f5afba74
ABHISHEK-AMRUTE <abhishekamrutelfs@gmail.com>
2021-07-02 21:37:07 +0530

    Hero asset added to home screen

--- src/App.js
+++ src/App.js
@@ -35,85 +35,87 @@ const useStyles = makeStyles((theme) => ({
 
 
 
-class App extends Component  {
+class App extends Component {
 
   state = {
-    viewVariable:0
+    viewVariable: 0
   };
-  aboutEventListner=(e) =>{
+  aboutEventListner = (e) => {
     console.log('clicked')
-   
+
 
     this.setState({
-      viewVariable : 0
+      viewVariable: 0
     })
   }
-   expEventListner=(e) =>{
- 
-    console.log('clicked')
-  
-
-    this.setState({
-      viewVariable : 1
-    })
-  }
-  projectEventListner=(e) =>{
+  expEventListner = (e) => {
 
     console.log('clicked')
-   
+
 
     this.setState({
-      viewVariable : 2
+      viewVariable: 1
     })
   }
-   contactEventListner=(e) =>{
- 
+  projectEventListner = (e) => {
+
     console.log('clicked')
-   
+
 
     this.setState({
-      viewVariable : 3
+      viewVariable: 2
     })
   }
-  drilink =(e) => window.open('https://dribbble.com/abhishek_bt18','_blank');
-  githublink =(e) => window.open('https://github.com/ABHISHEK-AMRUTE','_blank');
-  gpslink =(e) => window.open('https://play.google.com/store/apps/developer?id=ABHISHEK+AMRUTE&hl=en_IN&gl=US','_blank');
-  linkedinLink =(e) => window.open('https://play.google.com/store/apps/developer?id=ABHISHEK+AMRUTE&hl=en_IN&gl=US','_blank');
-  
+  contactEventListner = (e) => {
+
+    console.log('clicked')
+
+
+    this.setState({
+      viewVariable: 3
+    })
+  }
+  drilink = (e) => window.open('https://dribbble.com/abhishek_bt18', '_blank');
+  githublink = (e) => window.open('https://github.com/ABHISHEK-AMRUTE', '_blank');
+  gpslink = (e) => window.open('https://play.google.com/store/apps/developer?id=ABHISHEK+AMRUTE&hl=en_IN&gl=US', '_blank');
+  linkedinLink = (e) => window.open('https://play.google.com/store/apps/developer?id=ABHISHEK+AMRUTE&hl=en_IN&gl=US', '_blank');
+
 
   // const classes = useStyles();
 
-  render(){
+  render() {
     return (
       <div className="App">
-        <img src={bodyHeroAsset} className="bodyHeroAsset"></img>
+     <img src={bodyHeroAsset} className="bodyHeroAsset"></img>
         <Grid container justify="center" spacing={3}>
           <Grid item>
             <div onClick={this.aboutEventListner}>
               <Buttons text="About" />
-              { this.state.viewVariable == 0 ? (<div className="horibar"> </div>):(<div/>)}
+              {this.state.viewVariable == 0 ? (<div className="horibar"> </div>) : (<div />)}
             </div>
           </Grid>
           <Grid item>
             <div onClick={this.expEventListner}>
               <Buttons text="Experience" />
-              { this.state.viewVariable == 1 ? (<div className="horibar"> </div>):(<div/>)}
+              {this.state.viewVariable == 1 ? (<div className="horibar"> </div>) : (<div />)}
             </div>
           </Grid>
           <Grid item>
             <div onClick={this.projectEventListner}>
               <Buttons text="Projects" />
-              { this.state.viewVariable == 2 ? (<div className="horibar"> </div>):(<div/>)}
+              {this.state.viewVariable == 2 ? (<div className="horibar"> </div>) : (<div />)}
             </div>
           </Grid>
           <Grid item>
             <div onClick={this.contactEventListner}>
               <Buttons text="Contact" />
-              { this.state.viewVariable == 3 ? (<div className="horibar"> </div>):(<div/>)}
+              {this.state.viewVariable == 3 ? (<div className="horibar"> </div>) : (<div />)}
             </div>
           </Grid>
         </Grid>
-  
+      
+
+
         {/* <AppBar position="static">
           <Toolbar>
             <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
@@ -125,38 +127,36 @@ class App extends Component  {
             <Button color="inherit">Abhishek Amrute</Button>
           </Toolbar>
         </AppBar> */}
-       { this.state.viewVariable==0?(<About></About>):(<div/>)}
-       { this.state.viewVariable==2?( <Projects></Projects>):(<div/>)}
-       { this.state.viewVariable==1?(  <Experience></Experience>):(<div/>)}
+        {this.state.viewVariable == 0 ? (<About></About>) : (<div />)}
+        {this.state.viewVariable == 2 ? (<Projects></Projects>) : (<div />)}
+        {this.state.viewVariable == 1 ? (<Experience></Experience>) : (<div />)}
 
-       {/* <About/>
+        {/* <About/>
        <Projects/>
        <Experience/>
         */}
-      <div className="topMargin">
+        <div className="topMargin">
 
-<Grid container justify="center" spacing={3}>
-    
-    
-    <Grid item>
-        <img onClick={this.linkedinLink} src={linkedin} className="imageClass"></img>
-    </Grid>
-    <Grid item>
-        <img onClick={this.githublink} src={github} className="imageClass"></img>
-    </Grid>
-    <Grid item>
-        <img onClick={this.drilink} src={dribbble} className="imageClass"></img>
-    </Grid>
-    
-    <Grid item>
-        <img onClick={this.gpslink} src={googlePlayStore} className="imageClass"></img>
-    </Grid>
+          <Grid container justify="center" spacing={3}>
+
+
+            <Grid item>
+              <img onClick={this.linkedinLink} src={linkedin} className="imageClass"></img>
+            </Grid>
+            <Grid item>
+              <img onClick={this.githublink} src={github} className="imageClass"></img>
+            </Grid>
+            <Grid item>
+              <img onClick={this.drilink} src={dribbble} className="imageClass"></img>
+            </Grid>
+
+            <Grid item>
+              <img onClick={this.gpslink} src={googlePlayStore} className="imageClass"></img>
+            </Grid>
+
+          </Grid>
+        </div>
      
-</Grid>
-</div>
-
-     
-
 
       </div>
     );
